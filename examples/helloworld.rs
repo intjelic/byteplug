e4e0ddfc83b29b94bf14fae6476f420edb56d3d2
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2019-12-11 03:22:39 +0700

    Added a do nothing 'helloworld' example to be implemented later

---
+++ examples/helloworld.rs
@@ -0,0 +1,12 @@
+use distill::geometry::*;
+use distill::renderer::*;
+use distill::animation::*;
+use distill::audio::*;
+use distill::video::*;
+use distill::input::*;
+use distill::application::*;
+use distill::ui::*;
+
+fn main() {
+    println!("Hello Hello World example!")
+}

858e0e99d1bb7106b5357d8d1e120637a48ba35a
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2019-12-11 02:00:34 +0700

    Placed the project under the MIT license

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2020 - BytePlug
+//
+// This source file is part of Distilled Multimedia Library which is released
+// under the MIT license. Please refer to the LICENSE file that can be found
+// at the root of the project directory.
+//
+// Written by Jonathan De Wachter <dewachter.jonathan@gmail.com>, January 2020
+
 pub mod renderer {
     struct Color {}
     struct Gradient {}

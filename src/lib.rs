e19179d6ab7aceb7d8762f2342c2686040005a55
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2019-12-11 15:31:04 +0700

    Wrote documentation of the crate item

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,22 @@
 //
 // Written by Jonathan De Wachter <dewachter.jonathan@gmail.com>, January 2020
 
+//! # Distilled Multimedia Library
+//!
+//! **Distill** is a multimedia library for the Rust language which aims to
+//! provide a decent framework to write applications (or games) for desktop (or
+//! mobile).
+//!
+//! While it appears to feature tools in many areas (animation, ui, etc.), it
+//! actually provides the bare minimum and each are carefully crafted to be
+//! extended and integrate well with external libraries.
+//! For instance, if you had a bad experience with the thousands of poorly
+//! designed GUI toolkits out there, you might be plaisantly surprised by the
+//! **ui** momdule.
+//!
+//! It was heavily inspired by similar framework (like SFML) and other
+//! extensions developed by the community (Thor) coupled with my decade of
+//! experience in the field and my relentless perfectionism.
 pub mod geometry;
 pub mod renderer;
 pub mod animation;

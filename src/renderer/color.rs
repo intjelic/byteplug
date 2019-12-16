220945b079fbbabac1d683d8c36fa9af3fa0f9f6
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2019-12-16 18:23:42 +0700

    Implemented simple color (default constructor, comparable, printable, copy semantic)

--- src/renderer/color.rs
+++ src/renderer/color.rs
@@ -9,4 +9,21 @@
 /// Brief description
 ///
 /// Long decripiton.
-pub struct Color {}
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct Color {
+    pub red:   u8,
+    pub green: u8,
+    pub blue:  u8,
+    pub alpha: u8
+}
+
+impl Color {
+    pub fn new() -> Color {
+        Color{
+            red:   0,
+            green: 0,
+            blue:  0,
+            alpha: 255
+        }
+    }
+}

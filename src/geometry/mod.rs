6e4fc0e0c1dc159cdff19dc1d0a3071210fa6d53
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2020-06-17 17:57:51 +0700

    Wrote description of the geometry module

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -7,7 +7,32 @@
 
 //! Mathematical utilities related to geometry
 //!
-//! Additional documentation is to be written here.
+//! The geometry module provides the basics utilities to work in the Euclidean space, and is crafted
+//! to go in pair with the draw module. It provides the base components that is used by almost all
+//! the other modules such positions and sizes.
+//!
+//! It's a light version of highly mathematical oriented geometry crates out there, more affordable
+//! for less mathematically-inclined programmers. It strips items from their complex mathematical
+//! concepts that is either not relevant or hardly ever needed for our particular context.
+//! **Module structure**
+//!
+//! Items in this module can be mentally divided into different categories.
+//!
+//! - Position, Size and Box items
+//! - Vector and Matrix items
+//! - Transform item
+//! - Movable, Resizable, Rotable and Scalable items
+//! - Point, Line, Rectangle, Circle and Polygon items
+//!
+//! Positions, sizes and boxes are the base components, used by almost all the other modules of this
+//! crate; they are self-explanatory, except box, which simply is a rectangle (a position and a
+//! size). Vectors and matrices are two mathematical primitives that allow to do useful geometrical
+//! calculation such as transforming positions; they work together. Transform is a higher-level
+//! primitive that encapsulates a matrix to transform position and boxes.
+//! Movable, resizable, rotable and scalable are traits that user-defined items can implement when
+//! it supports some common geometrical operations. Points, lines, rectangles, circles and polygons
+//! items hold geometrical information and allow to compute collision detection.
+//!
 mod utils;
 
 mod position;
